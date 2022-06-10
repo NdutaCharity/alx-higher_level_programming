ce7d9d322623b9fb5f5e854638698c3f87074800
NdutaCharity <58165250+NdutaCharity@users.noreply.github.com>
2022-06-10 15:35:47 +0300

    Update 9-print_last_digit.py

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,6 +1,11 @@
-#!/usr/bin/env python3
-print_last_digit = __import__('9-print_last_digit').print_last_digit
-print_last_digit(98)
-print_last_digit(0)
-r = print_last_digit(-1024)
-print(r)
+#!/usr/bin/python3
+def print_last_digit(number):
+    exe = 0
+    if number < 0:
+        number *= -1
+    exe = 1
+    last = number % 10
+    if exe == 1:
+        number *= -1
+    print("{:d}".format(last), end="")
+    return last
